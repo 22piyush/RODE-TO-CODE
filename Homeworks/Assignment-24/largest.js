901b47bff306decbfb7026b65d68c192872b746e
22piyush <piyushaglawe05@gmail.com>
2024-06-14 16:58:13 +0530

    largest number program completed

---
+++ Homeworks/Assignment-24/largest.js
@@ -0,0 +1,12 @@
+let number1 = prompt("Enter First Number");
+let number2= prompt("Enter Second Number");
+
+if(number1>number2)
+    {
+        console.log(`${number1} is greater`);
+    }
+    else{
+
+        console.log(`${number2} is greater`);
+
+    }
